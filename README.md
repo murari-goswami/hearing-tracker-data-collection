f1080edff1105540c2a9613c0e4a99f95e766d48
Murari Mohan Goswami <murarimohan.goswami@wsa.com>
2021-08-19 18:48:38 +0200

    incremental

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hearing-tracker-data-collection
+Hearing tracker https://www.hearingtracker.com/ is a rich forum for discussions on various brands from hearing aid industry. This is place where patients/ customers talk 
+about different topics and gives their feedbacks.
+This project scrapes data from the forum and load into a dataframe and eventually into spark tables.
--- main.py
+++ main.py
@@ -19,14 +19,16 @@ from pyspark.sql.functions import explode, col
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 import logging
 
-from sklearn.metrics import accuracy_score,hamming_loss,classification_report
+from sklearn.metrics import accuracy_score, hamming_loss, classification_report
 
-### Split Dataset into Train and Text
+# Split Dataset into Train and Text
+
 from sklearn.model_selection import train_test_split
+
 # Feature engineering
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-#from skmultilearn.adapt import MLkNN
+# from skmultilearn.adapt import MLkNN
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -45,10 +47,9 @@ from ktrain import text
 
 import matplotlib.pyplot as plt
 
-from loguru import logger # https://github.com/Delgan/loguru
+from loguru import logger  # https://github.com/Delgan/loguru
 
 log_level = "info"
 
-if __name__ == '__main__':        
+if __name__ == '__main__':
     logger.info("main Function")
-
